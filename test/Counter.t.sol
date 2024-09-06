477b33bd4fdc2834903c8e9f1ce67fdde80c1bd8
Easy Rider <zhadkov@gmail.com>
2024-09-06 19:19:16 +0300

    Initial commit

---
+++ test/Counter.t.sol
@@ -0,0 +1,115 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import "forge-std/Test.sol";
+import {IHooks} from "v4-core/src/interfaces/IHooks.sol";
+import {Hooks} from "v4-core/src/libraries/Hooks.sol";
+import {TickMath} from "v4-core/src/libraries/TickMath.sol";
+import {IPoolManager} from "v4-core/src/interfaces/IPoolManager.sol";
+import {PoolKey} from "v4-core/src/types/PoolKey.sol";
+import {BalanceDelta} from "v4-core/src/types/BalanceDelta.sol";
+import {PoolId, PoolIdLibrary} from "v4-core/src/types/PoolId.sol";
+import {CurrencyLibrary, Currency} from "v4-core/src/types/Currency.sol";
+import {PoolSwapTest} from "v4-core/src/test/PoolSwapTest.sol";
+import {Counter} from "../src/Counter.sol";
+import {StateLibrary} from "v4-core/src/libraries/StateLibrary.sol";
+import {PositionConfig} from "v4-periphery/src/libraries/PositionConfig.sol";
+
+import {IPositionManager} from "v4-periphery/src/interfaces/IPositionManager.sol";
+import {EasyPosm} from "./utils/EasyPosm.sol";
+import {Fixtures} from "./utils/Fixtures.sol";
+
+contract CounterTest is Test, Fixtures {
+    using EasyPosm for IPositionManager;
+    using PoolIdLibrary for PoolKey;
+    using CurrencyLibrary for Currency;
+    using StateLibrary for IPoolManager;
+
+    Counter hook;
+    PoolId poolId;
+
+    uint256 tokenId;
+    PositionConfig config;
+
+    function setUp() public {
+        // creates the pool manager, utility routers, and test tokens
+        deployFreshManagerAndRouters();
+        deployMintAndApprove2Currencies();
+
+        deployAndApprovePosm(manager);
+
+        // Deploy the hook to an address with the correct flags
+        address flags = address(
+            uint160(
+                Hooks.BEFORE_SWAP_FLAG | Hooks.AFTER_SWAP_FLAG | Hooks.BEFORE_ADD_LIQUIDITY_FLAG
+                    | Hooks.BEFORE_REMOVE_LIQUIDITY_FLAG
+            ) ^ (0x4444 << 144) // Namespace the hook to avoid collisions
+        );
+        bytes memory constructorArgs = abi.encode(manager); //Add all the necessary constructor arguments from the hook
+        deployCodeTo("Counter.sol:Counter", constructorArgs, flags);
+        hook = Counter(flags);
+
+        // Create the pool
+        key = PoolKey(currency0, currency1, 3000, 60, IHooks(hook));
+        poolId = key.toId();
+        manager.initialize(key, SQRT_PRICE_1_1, ZERO_BYTES);
+
+        // Provide full-range liquidity to the pool
+        config = PositionConfig({
+            poolKey: key,
+            tickLower: TickMath.minUsableTick(key.tickSpacing),
+            tickUpper: TickMath.maxUsableTick(key.tickSpacing)
+        });
+        (tokenId,) = posm.mint(
+            config,
+            10_000e18,
+            MAX_SLIPPAGE_ADD_LIQUIDITY,
+            MAX_SLIPPAGE_ADD_LIQUIDITY,
+            address(this),
+            block.timestamp,
+            ZERO_BYTES
+        );
+    }
+
+    function testCounterHooks() public {
+        // positions were created in setup()
+        assertEq(hook.beforeAddLiquidityCount(poolId), 1);
+        assertEq(hook.beforeRemoveLiquidityCount(poolId), 0);
+
+        assertEq(hook.beforeSwapCount(poolId), 0);
+        assertEq(hook.afterSwapCount(poolId), 0);
+
+        // Perform a test swap //
+        bool zeroForOne = true;
+        int256 amountSpecified = -1e18; // negative number indicates exact input swap!
+        BalanceDelta swapDelta = swap(key, zeroForOne, amountSpecified, ZERO_BYTES);
+        // ------------------- //
+
+        assertEq(int256(swapDelta.amount0()), amountSpecified);
+
+        assertEq(hook.beforeSwapCount(poolId), 1);
+        assertEq(hook.afterSwapCount(poolId), 1);
+    }
+
+    function testLiquidityHooks() public {
+        // positions were created in setup()
+        assertEq(hook.beforeAddLiquidityCount(poolId), 1);
+        assertEq(hook.beforeRemoveLiquidityCount(poolId), 0);
+
+        // remove liquidity
+        uint256 liquidityToRemove = 1e18;
+        posm.decreaseLiquidity(
+            tokenId,
+            config,
+            liquidityToRemove,
+            MAX_SLIPPAGE_REMOVE_LIQUIDITY,
+            MAX_SLIPPAGE_REMOVE_LIQUIDITY,
+            address(this),
+            block.timestamp,
+            ZERO_BYTES
+        );
+
+        assertEq(hook.beforeAddLiquidityCount(poolId), 1);
+        assertEq(hook.beforeRemoveLiquidityCount(poolId), 1);
+    }
+}
